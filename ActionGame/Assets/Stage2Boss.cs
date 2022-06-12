e5ca1fca1e667aa684075798533985923a13c879
carrot4343 <carrot4343@gmail.com>
2022-06-12 23:22:01 +0900

    stage2 alpha
    asd

---
+++ ActionGame/Assets/Stage2Boss.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage2Boss : MonoBehaviour
+{
+    public int bossHealthPoint;
+    void Start()
+    {
+        bossHealthPoint = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bossHealthPoint == 0)
+        {
+            //SceneManager.LoadScene("Stage 3");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bullet")
+        {
+            bossHealthPoint -= 1;
+        }
+    }
+}
